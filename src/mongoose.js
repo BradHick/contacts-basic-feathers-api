3c6d78bb5c9059dc019937f96136b53202d96160
BradHick <bradleyhrm@gmail.com>
2018-08-24 10:56:21 -0300

    refatorando conexão

--- src/mongoose.js
+++ src/mongoose.js
@@ -1,11 +1,7 @@
 const mongoose = require('mongoose');
 
 module.exports = function (app) {
-  if(process.env.MONGOLAB_URI){
-    mongoose.connect(process.env.MONGOLAB_URI, {});
-  }else{
-    mongoose.connect(app.get('mongodb'), {});
-  }
+  mongoose.connect(process.env.MONGOLAB_URI || app.get('mongodb'), {});
   mongoose.Promise = global.Promise;
 
   app.set('mongooseClient', mongoose);
