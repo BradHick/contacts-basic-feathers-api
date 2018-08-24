97d293f196c43b3860f63cf6e8cfceca952b244e
BradHick <bradleyhrm@gmail.com>
2018-08-24 10:32:20 -0300

    Modificações para fazer deploy no heroku

--- src/mongoose.js
+++ src/mongoose.js
@@ -1,7 +1,11 @@
 const mongoose = require('mongoose');
 
 module.exports = function (app) {
-  mongoose.connect(app.get('mongodb'), {});
+  if(process.env.MONGOLAB_URI){
+    mongoose.connect(process.env.MONGOLAB_URI, {});
+  }else{
+    mongoose.connect(app.get('mongodb'), {});
+  }
   mongoose.Promise = global.Promise;
 
   app.set('mongooseClient', mongoose);
